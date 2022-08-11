15532dfa8f32f1eccc5c8697a35f760b791974ec
hsw510 <hsw510@gmail.com>
2022-08-11 18:41:23 +0900

    adapter

--- src/adapter/ducks/TurkeyAdapter.java
+++ src/adapter/ducks/TurkeyAdapter.java
@@ -1,5 +1,9 @@
 package adapter.ducks;
 
+/**
+ * 한 클래스의 인터페이스를 클라이언트에서 사용하고자 하는 다른 인터페이스로 변환합니다.
+ * 어댑터를 이용하면 인터페이스 호환성 문제 때문에 같이 쓸 수 없는 클래스들을 연결해서 쓸 수 있습니다.
+ */
 public class TurkeyAdapter implements Duck { //Duck Target Inter
 	Turkey turkey; // Adaptee 위임, 구성(composite)방식
  
