76eed90ffbe41750f1c92cce8a65469aad9a40a0
hsw510 <hsw510@gmail.com>
2022-08-11 18:39:19 +0900

    adapter

--- src/adapter/ducks/TurkeyAdapter.java
+++ src/adapter/ducks/TurkeyAdapter.java
@@ -1,7 +1,7 @@
 package adapter.ducks;
 
-public class TurkeyAdapter implements Duck {
-	Turkey turkey;
+public class TurkeyAdapter implements Duck { //Duck Target Inter
+	Turkey turkey; // Adaptee 위임, 구성(composite)방식
  
 	public TurkeyAdapter(Turkey turkey) {
 		this.turkey = turkey;
